80efbc3faa78b64b38b711f2071f7ddc23931791
Aihtul <115374380+arihtul@users.noreply.github.com>
2023-10-04 14:09:51 +0530

    Create sum.php

---
+++ Mini-Projects/PHP/sum.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Addition in PHP</title>
+</head>
+<body>
+    <?php
+    // PHP code starts here
+    $num1 = 5;
+    $num2 = 7;
+    $sum = $num1 + $num2;
+    
+    echo "The sum of $num1 and $num2 is: $sum";
+    // PHP code ends here
+    ?>
+</body>
+</html>
