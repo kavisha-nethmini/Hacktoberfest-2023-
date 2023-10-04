6f7260edeb83ff513bd001bf12e7c21570d45162
Luthira Geesilu <91755686+Geesilu@users.noreply.github.com>
2023-10-04 14:45:43 +0530

    Create PowerCalculator.java

---
+++ Mini-Projects/JAVA/PowerCalculator.java
@@ -0,0 +1,13 @@
+public class PowerCalculator {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 5;
+        long result = 1;
+        
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
